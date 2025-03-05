efcac5691c94a35a4bb61ceeb4e1841bc3e5f5ac
UemuraNeo <ikumi.uemura@neosystem.co.jp>
2025-03-05 17:47:34 +0900

    軽微な訂正

--- api/dto/item_dto.go
+++ api/dto/item_dto.go
@@ -1,7 +1,7 @@
 package dto
 
 type CreateItemInput struct {
-	Name        string `json: "name" binding:"required, min=2"`
+	Name        string `json:"name" binding:"required, min=2"`
 	Price       int    `json:"price" binding:"required,min=1,max=999999"`
 	Description string `json:"description"`
 }
